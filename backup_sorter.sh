2718aff5beec76c5e8cd81767a9640ef49274518
Amin <Aminuxer@users.noreply.github.com>
2017-08-29 18:51:23 +0300

    Add files via upload

---
+++ backup_sorter.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -d "$1" ]
+   then wdir=$1;
+   else
+     echo "Usage: $0 <Dir for Proxmox backup sort>";
+     exit 1;
+fi
+
+echo "Work Dir is $wdir";
+
+for logf in $wdir/vzdump-*.log;
+do
+   vmname=`cat $logf | grep Name | cut -d ' ' -f 7 | head -n 1`
+   vmtype=`cat $logf | grep Name | cut -d ' ' -f 5 | head -n 1`
+   fnpref=`echo "$logf" | cut -d '.' -f 1`
+
+   if [ ! -n "$vmname" ] || [ ! -n "$vmtype" ] || [ ! -n "$fnpref" ]
+      then echo "No VM Name/Type in $logf file";
+      exit 2;
+   fi
+
+   echo "$vmtype $vmname";
+   mkdir -p $wdir/$vmtype/$vmname
+   mv $fnpref* $wdir/$vmtype/$vmname
+
+done
